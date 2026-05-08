ecdb318357d6e4e2273c16c3f45d28a261bcd7aa
Rosa-cher <2679838366@qq.com>
2026-05-08 18:00:57 +0800

    Create index.js

---
+++ src/domain/index.js
@@ -0,0 +1,6 @@
+// 导出作业要求的所有接口
+export { createSudoku, createSudokuFromJSON } from './Sudoku.js';
+export { createGame, createGameFromJSON } from './Game.js';
+// 导出类（方便 Store 中使用）
+export { Sudoku, Game } from './Sudoku.js';
+export { Game } from './Game.js';
