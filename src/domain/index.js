257d036d9e64790d9449bd17683f751e288ec708
Rosa-cher <2679838366@qq.com>
2026-05-08 18:47:40 +0800

    Update index.js

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,6 +1,19 @@
-// 导出作业要求的所有接口
-export { createSudoku, createSudokuFromJSON } from './Sudoku.js';
-export { createGame, createGameFromJSON } from './Game.js';
-// 导出类（方便 Store 中使用）
-export { Sudoku, Game } from './Sudoku.js';
-export { Game } from './Game.js';
+// src/domain/index.js
+import { Sudoku } from './Sudoku';
+import { Game } from './Game';
+
+export function createSudoku(grid) {
+  return new Sudoku(grid);
+}
+
+export function createSudokuFromJSON(json) {
+  return Sudoku.fromJSON(json);
+}
+
+export function createGame({ sudoku }) {
+  return new Game(sudoku);
+}
+
+export function createGameFromJSON(json) {
+  return Game.fromJSON(json);
+}
